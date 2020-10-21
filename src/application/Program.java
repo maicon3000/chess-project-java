dae6a0b6f960716089bc1284c8072bcea3899f24
maicon3000 <maicon3000@hotmail.com>
2020-10-21 20:21:58 -0300

    Criação do layer de xadrez e impressão do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,13 @@
 package application;
 
-import java.util.Scanner;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		sc.close();
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
