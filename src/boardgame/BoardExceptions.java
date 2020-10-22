e2565ae3102b11fab4a7fe3a9d3173ea49a72082
maicon3000 <maicon3000@hotmail.com>
2020-10-22 16:31:51 -0300

    BoardException e programação defensiva

---
+++ src/boardgame/BoardExceptions.java
@@ -0,0 +1,10 @@
+package boardgame;
+
+public class BoardExceptions extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public BoardExceptions(String msg) {
+		super(msg);
+	}
+	
+}
